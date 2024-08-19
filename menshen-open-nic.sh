98fdf4f217a85ada9106aeda24fbcfcdcff1c2e1
Alessandro Vacca <alessandro2.vacca@mail.polimi.it>
2024-08-19 18:12:28 +0200

    buildsystem

---
+++ menshen-open-nic.sh
@@ -0,0 +1,25 @@
+# MENSHEN
+[ -d "menshen" ] && rm -rf "menshen"
+[ -d "rmtv2" ] && rm -rf "rmtv2"
+git clone https://github.com/multitenancy-project/menshen.git
+mv menshen/lib_rmt/rmtv2/ rmtv2
+mv menshen/lib_rmt/netfpga_fifo/fallthrough_small_fifo_v1_0_0/hdl/fallthrough_small_fifo.v rmtv2/
+mv menshen/lib_rmt/netfpga_fifo/fallthrough_small_fifo_v1_0_0/hdl/small_fifo.v rmtv2/
+rm -rf menshen
+mv rmtv2 menshen
+cp opennic_integration.tcl menshen/tcl/
+cp tbs/tb_rmt_wrapper_calc.sv menshen/tb/
+cp tbs/tb_rmt_wrapper_drop.sv menshen/tb/
+cp tbs/tb_rmt_wrapper_if_else.sv menshen/tb/
+cp tbs/tb_rmt_wrapper_modules.sv menshen/tb/
+cp tbs/tb_rmt_wrapper_stages.sv menshen/tb/
+
+# ON SHELL
+[ -d "open-nic-shell" ] && rm -rf "open-nic-shell"
+git clone https://github.com/Xilinx/open-nic-shell.git
+patch open-nic-shell/script/build.tcl < open-nic-shell-patches/build.patch
+patch open-nic-shell/src/open_nic_shell.sv < open-nic-shell-patches/open_nic_shell.patch
+patch open-nic-shell/src/open_nic_shell_macros.vh < open-nic-shell-patches/open_nic_shell_macros.patch
+realpath open-nic-tbs
+VAR=$(realpath open-nic-tbs)
+sed -i "" "s|{{VAR}}|${VAR}|g" "open-nic-shell/script/build.tcl"
